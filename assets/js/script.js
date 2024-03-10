b5d9742c07a9d77a1ff79be977144c0fb5bce83e
Hussam Azzuni <83836344+HussamAzzuni@users.noreply.github.com>
2024-03-10 14:52:32 +0400

    mod

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const circle = document.getElementById('circle');
-
-  document.addEventListener('mousemove', function(event) {
+  const bg = document.querySelector('#bg');
+  const radius = 50; // Adjust the radius as needed
+  
+  function updateBackground(event) {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
-    // Move the circle with the mouse cursor
-    circle.style.left = mouseX - circle.offsetWidth / 2 + 'px';
-    circle.style.top = mouseY - circle.offsetHeight / 2 + 'px';
-  });
+    // Calculate the range of pixels to be affected
+    const startX = Math.max(0, mouseX - radius);
+    const startY = Math.max(0, mouseY - radius);
+    const endX = Math.min(window.innerWidth, mouseX + radius);
+    const endY = Math.min(window.innerHeight, mouseY + radius);
+
+    // Apply transformation to the background pixels within the specified radius
+    bg.style.backgroundPosition = `-${startX}px -${startY}px`;
+  }
+
+  document.addEventListener('mousemove', updateBackground);
 });
